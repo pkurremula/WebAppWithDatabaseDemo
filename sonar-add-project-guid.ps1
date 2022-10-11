e55488d900f7516d9a6fd5ca299cb38e5c904b6f
Prabhanjan Kurremula <pkurremula@gmail.com>
2022-10-10 23:31:21 -0400

    Update sonar-add-project-guid.ps1

--- sonar-add-project-guid.ps1
+++ sonar-add-project-guid.ps1
@@ -7,9 +7,11 @@ $paths = Get-ChildItem -include *.csproj -Recurse
 foreach($pathobject in $paths) 
 {
     $path = $pathobject.fullname
+    Write-Output $path
     $doc = New-Object System.Xml.XmlDocument
     $doc.Load($path)
     $child = $doc.CreateElement("ProjectGuid")
+    Write-Output $child
     $child.InnerText = [guid]::NewGuid().ToString().ToUpper()
     $node = $doc.SelectSingleNode("//Project/PropertyGroup")
     $node.AppendChild($child)
